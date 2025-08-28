551ebb3f23ae996d1376ffe2cf7154f36c87d54a
Simone Erba <simone.erba@oncode.it>
2025-08-28 22:28:00 +0200

    edge edit

--- backend/src/main/java/com/flumen/backend/BackendApplication.java
+++ backend/src/main/java/com/flumen/backend/BackendApplication.java
@@ -4,13 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.orientechnologies.orient.core.db.OrientDB;
-import com.orientechnologies.orient.core.db.OrientDBConfig;
-import com.orientechnologies.orient.core.metadata.schema.OClass;
-import com.orientechnologies.orient.core.record.OVertex;
-import com.orientechnologies.orient.core.sql.executor.OResult;
-import com.orientechnologies.orient.core.sql.executor.OResultSet;
-
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
